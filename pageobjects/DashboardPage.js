aa19164905602e7433359714972489a05bcdcc36
rich <bla@bla.com>
2024-07-19 22:17:16 -0600

    Added Dashboard Spec with two methods that click Parent and Child Sidebar Buttons

--- pageobjects/DashboardPage.js
+++ pageobjects/DashboardPage.js
@@ -6,12 +6,24 @@ class DashboardPage
     {
         this.page = page;
         this.searchBox = page.locator('input[name="q"]').first();
+        this.sidebarList = page.locator('li');
     }
     
+    async clickParentSideBarButton(parentButtonName)
+    {
+        const button = this.sidebarList.locator('.nav-link-title', { hasText: parentButtonName }).first();
+        await button.click();
+    }
+
+    async clickChildSideBarButton(childButtonName)
+    {
+        const button = this.sidebarList.locator('.dropdown-menu.show .dropdown-item', { hasText: childButtonName}).first();
+        await button.click();
+    }
+
     async validateDashboardPageLoaded()
     {
         await expect(this.searchBox).toBeEditable();
-        
     }
 }
 module.exports = {DashboardPage}
--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -1,10 +1,12 @@
 const {test, expect} = require('@playwright/test')
 const { POManager } = require('../pageobjects/POManager');
 
-test("Netbox Login", async ({page}) => {
+test.only("Navigate Sidebar", async ({page}) => {
 
     const USERNAME = "bitshredr";
     const PASSWORD = "!T0mmygun***";
+    const PARENT_SIDEBAR_TEXT = "VPN";
+    const CHILD_SIDEBAR_TEXT = "L2VPNs";
 
     const poManager = new POManager(page);
     const loginPage = poManager.getLoginPage();
@@ -13,6 +15,8 @@ test("Netbox Login", async ({page}) => {
     await page.goto("http://127.0.0.1:8000");
     await loginPage.validLogin(USERNAME, PASSWORD);
     await dashboardPage.validateDashboardPageLoaded();
+    await dashboardPage.clickParentSideBarButton(PARENT_SIDEBAR_TEXT);
+    await dashboardPage.clickChildSideBarButton(CHILD_SIDEBAR_TEXT);
 
     await page.pause();
 });
